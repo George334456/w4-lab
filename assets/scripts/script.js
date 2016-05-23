ddacd2be5372247090053e78fadfc1d2f13a8ff0
CSC309lecturer <katie.seaborn@mail.utoronto.ca>
2016-05-23 16:58:38 -0400

    updated script

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,18 +1,18 @@
 window.onload = function() {
     
     var c = document.getElementById("main");
-    var ctx = c.getContext("2d");
+    window.ctx = c.getContext("2d"); // Dealing with a global context is easier
     
     draw();
     
-    //animateO;
+    //animate();
     
 } // window.onload
 
 
 
 // Let's create a draw function for the canvas
-function draw () {
+function draw() {
     
     // Draw here
     
@@ -23,7 +23,7 @@ function draw () {
 function animate() {
     
     // Always clear the canvas after drawing each frame
-    ctx.clearRect(0, 0, 640, 480);
+    window.ctx.clearRect(0, 0, 640, 480);
     
     // Draw here, including conditionals
     
