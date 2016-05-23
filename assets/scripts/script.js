2bc6e7f7cbd75e6d4157515c40bff40d0f27da4e
CSC309lecturer <katie.seaborn@mail.utoronto.ca>
2016-05-23 16:04:39 -0400

    Updated script

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,8 @@
 window.onload = function() {
     
+    var c = document.getElementById("main");
+    var ctx = c.getContext("2d");
+    
     draw();
     
     //animateO;
@@ -11,7 +14,7 @@ window.onload = function() {
 // Let's create a draw function for the canvas
 function draw () {
     
-    // Your drawing code here
+    // Draw here
     
 }
 
@@ -19,7 +22,11 @@ function draw () {
 // Let's animate!
 function animate() {
     
-    // Your animations here
+    // Always clear the canvas after drawing each frame
+    ctx.clearRect(0, 0, 640, 480);
     
+    // Draw here, including conditionals
+    
+    // This will run animate() every 33 ms
     setTimeout(animate, 33);
 }
