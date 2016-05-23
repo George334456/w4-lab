4f5ab55476f4d6d9d6b4f6b42443f910887f73d5
CSC309lecturer <katie.seaborn@mail.utoronto.ca>
2016-05-23 14:19:13 -0400

    First w4 l3 commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    
+    draw();
+    
+} // window.onload
+
+
+
+// Let's create a draw function for the canvas
+function draw () {
+    
+    // Your code here
+    
+}
